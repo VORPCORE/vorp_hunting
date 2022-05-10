6443f3ff1fb1c1ec289cb2019b8a0e71efbbc994
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 19:05:47 +0100

    Update fxmanifest.lua

--- vorp_hunting/fxmanifest.lua
+++ vorp_hunting/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'VORP Core | rubi216 | Artzalez'
 description 'Hunting script for VORP Core'
-version 'alpha'
+
 
 client_script {
     'config.lua',
@@ -21,33 +21,8 @@ exports {
 	'DataViewNativeGetEventData'
 }
 
-fx_version 'adamant'
-game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'goncalobsccosta#9041'
 
-shared_scripts {
-  'config.lua'
-}
-
-client_scripts {
-  'client/Notifications.js',
-  'client/cl_*.lua',
-}
-
-server_scripts {
-  'server/class/sv_*.lua',
-  'server/sv_*.lua',
-}
-
-server_exports { 'vorpAPI' }
-
-files {
-  'ui/**/*'
-}
-
-ui_page 'ui/hud.html'
 
 --dont touch
 version '1.0'
