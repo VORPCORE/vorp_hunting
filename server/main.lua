73861a26dd199d1d5ea58304413e98efeb88ad36
AndrewR3K <andrew.martin.louis@gmail.com>
2022-05-30 22:23:47 -0700

    inventory check

--- server/main.lua
+++ server/main.lua
@@ -7,15 +7,24 @@ end)
 VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 
 RegisterServerEvent("vorp_hunting:giveReward")
-AddEventHandler("vorp_hunting:giveReward", function(givenItem, money, gold, rolPoints, xp, givenAmount)
+AddEventHandler("vorp_hunting:giveReward", function(givenItem, money, gold, rolPoints, xp, givenAmount, entity, horse, skipfinal)
     local _source = source
     local Character = VorpCore.getUser(_source).getUsedCharacter
+    local itemsAvailable = true 
+    local done = false
+
 
     if #givenItem ~= #givenAmount then
 
         print('Error: Please ensure givenItem and givenAmount have the same length in the items config.')
 
     elseif givenItem ~= nil then
+        local formattedGivenItems = {}
+        local total = 0
+
+        -- Format items and set random quantities if set.
+        -- Check if items can be added
+        -- total up the quantity so it can be checked as a whole
         for k, v in pairs(givenItem) do
             local nmb = 0
 
@@ -25,7 +34,41 @@ AddEventHandler("vorp_hunting:giveReward", function(givenItem, money, gold, rolP
                 nmb = math.random(Config.ItemQuantity.Min, Config.ItemQuantity.Max)
             end
 
-            VorpInv.addItem(_source, v, nmb)
+            formattedGivenItems[k] = {
+                nmb = nmb,
+                item = v
+            }
+
+            total = total + nmb
+
+            -- Check if there is enough to add, if not send message
+            TriggerEvent("vorpCore:canCarryItem", tonumber(_source), v, nmb, function(canCarryItem)                
+                if canCarryItem ~= true then
+                    itemsAvailable = false              
+                end
+                done = true
+            end)
+
+            while done == false do
+                Wait(500)
+            end
+        end
+
+        if itemsAvailable == false then
+            TriggerClientEvent("vorp:TipRight", _source, 'Inventory is too full to sell this animal', 4000)
+            return
+        end
+
+        -- Check if there is enough room in inventory in general.
+        local invAvailable = VorpInv.canCarryItems(_source, total)
+        if invAvailable ~= true then
+            TriggerClientEvent("vorp:TipRight", _source, 'Inventory is too full to sell this animal', 4000)
+            return
+        end
+
+        -- Give items
+        for k, v in pairs(formattedGivenItems) do
+            VorpInv.addItem(_source, v.item, v.nmb) 
         end
 
         if money ~= 0 then
@@ -42,6 +85,11 @@ AddEventHandler("vorp_hunting:giveReward", function(givenItem, money, gold, rolP
 
         if xp ~= 0 then
             Character.addXp(xp)
+        end
+        
+
+        if skipfinal ~= true then
+            TriggerClientEvent("vorp_hunting:finalizeReward", _source, entity, horse, money)
         end
     end
 end)
