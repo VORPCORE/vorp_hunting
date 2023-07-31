6374b8573c5bfe014f1a8ba5dc811fea64d10d94
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-07-31 02:54:26 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -32,8 +32,6 @@ Config.aiButcherped = true -- spawn ai butched ped set to false if you dont want
 
 Config.joblocked = false -- lock the butcher so only people with the job can access. u can change access to each butcher by editing this  butcherjob = "butcher"
 
-Config.maxpelts = 3 -- max pelts allowed on back of horse
-
 ------------------- Item Quantity Instructions -----------------
 -- The range for when a skinnableAnimal or Animal has a config value givenAmount of 0.
 -- Example: Animals with givenAmount = {0} can be sold to the butcher or be skinned, which will give the player a random number between 1 and 3 amount of givenItem
